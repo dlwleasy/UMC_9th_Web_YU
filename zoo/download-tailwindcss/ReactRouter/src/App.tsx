dd1c40149d273d113e49171c4b2f59a422e78df0
jiwon Lee <dlwleasy1@gmail.com>
2025-10-04 16:23:26 +0900

    [workbook]라우터 연결

--- zoo/download-tailwindcss/ReactRouter/src/App.tsx
+++ zoo/download-tailwindcss/ReactRouter/src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-// 1) 만든 페이지 import
 import HomePage from "./pages/home";
 import NotFound from "./pages/not-found";
 import Movies from "./pages/movies";
+//import RootLayout from "./layout/root-layout";
 
-// 2) 라우터에 연결
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+    //element: <RootLayout />,
     errorElement: <NotFound />,
   },
   {
