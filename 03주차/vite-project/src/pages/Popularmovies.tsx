a7532a5446045c42087c4def862739ca61937ffd
Navy22-anchor <lss4583@gmail.com>
2025-10-06 15:05:06 +0900

    Update Popularmovies.tsx

--- 03주차/vite-project/src/pages/Popularmovies.tsx
+++ 03주차/vite-project/src/pages/Popularmovies.tsx
@@ -23,7 +23,7 @@ const PopularMoviesPage = () => {
         `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${context?.PageNum}`,
         {
           headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTI4ZjdlMGFhMDE1YWIzNDM5MjU2ZjMyMzE3YzZjNyIsIm5iZiI6MTc1OTQ4NTgwOS43Mjg5OTk5LCJzdWIiOiI2OGRmOWY3MTE5NThjZTAwNzM0YzY3MTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mACp1NDgMWWsPGg2zqDS431cB0NTx6VYyGyVl-IBMd8`, // 본인 TMDB 토큰으로 교체
+            Authorization: `비밀`,
           },
         }
       );
