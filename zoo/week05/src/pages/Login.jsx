4c7ce23e2279f92002420ef04f1e20de4a148da6
jiwon Lee <dlwleasy1@gmail.com>
2025-11-05 08:12:51 +0900

    [feedback]Login 수정

--- zoo/week05/src/pages/Login.jsx
+++ zoo/week05/src/pages/Login.jsx
@@ -1,43 +1,56 @@
 import Navbar from "../components/Navbar.jsx";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
-  const [emailInput, setemailInput] = useState("");
-  //값이 바뀐다면 추적을 해야된다- 알아서 추적을 해준다.
-  //이벤트로써 확인할 수 있다.
+  const navigate = useNavigate();
+  //값 저장 -> 바뀌면 업그레이드 반복
+  //이벤트로써 onChange가 되면 값을 저장하고 이를 추적해주기 시작하면 된다.
+
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  //1.처음에는 값을 저장만 해줌
   const handleEmail = (e) => {
-    // return setemailInput(e.target.value);
-    //근데 여기서 값이 잘못 들어오면 바로 오류를 출력하고 완전히 되기 전에는 버튼을 활성화 시키지 않는다?
-
-    const input = e.target.value.trim();
-    if (input.includes("@") && input.includes(".")) setemailInput(input);
-    else alert("올바르게 이메일을 입력하세요.");
+    setEmail(e.target.value.trim());
   };
 
-  const [pwInput, setpwInput] = useState("");
   const handlePw = (e) => {
-    const input = e.target.value.trim();
-
-    if (input.length() > 6) setpwInput(input);
-    else console.log("비밀번호는 최소 6자 이상이어야 합니다");
+    setPw(e.target.value.trim());
   };
 
-  //로그인 상태 활성화 : 1의 조건을 만족한 경우 :
+  //2.값 검증
+  const email_valid = email.includes("@") && email.includes(".");
+  const pw_valid = pw.length() > 6;
+  const login_valid = email_valid && pw_valid;
 
-  const [active, setActive] = useState(false);
-  if (
-    emailInput.includes("@") &&
-    emailInput.includes(".") &&
-    pwInput.length > 6
-  )
-    setActive(true);
-  else setActive(false);
+  //   else alert("올바르게 이메일을 입력하세요.");console.log("비밀번호는 최소 6자 이상이어야 합니다");
+
+  //로그인 상태 활성화
+
+  //토큰
+  const loginToken = () => {
+    const loginURL = "http://localhost:8000/v1/auth/signin";
+    axios
+      .post(loginURL, { email: email, password: pw })
+      .then((response) => {
+        console.log(response.data);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("로그인 실패:", error);
+        alert("로그인 중 오류가 발생했습니다.");
+      });
+  };
 
   return (
     <>
       <Navbar></Navbar>
       <div className="login-header">
-        <button className="login-header-back">◀</button>
+        <button className="login-header-back" onClick={() => navigate(-1)}>
+          ◀
+        </button>
         <h2 className="login-header-title">로그인</h2>
       </div>
 
@@ -46,19 +59,29 @@ export default function Login() {
       <div className="login">
         <input
           className="login-email"
-          value={emailInput}
+          value={email}
           onChange={handleEmail}
           type="text"
           placeholder="이메일을 입력하세요!"
         ></input>
+        {email_valid == false && (
+          <span className="alert ID">이메일 형식이 아닙니다.</span>
+        )}
         <input
           className="login-password"
-          value={pwInput}
+          value={pw}
           onChange={handlePw}
           type="text"
           placeholder="비밀번호를 입력하세요!"
         ></input>
-        <button className="login-button" onChange={active}>
+        {pw_valid == false && (
+          <span className="alert ID">이메일 형식이 아닙니다.</span>
+        )}
+        <button
+          className="login-button"
+          disabled={!login_valid}
+          onClick={loginToken}
+        >
           로그인
         </button>
       </div>
