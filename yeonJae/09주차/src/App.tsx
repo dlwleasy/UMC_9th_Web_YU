50fcb84bcedc9f098f973a4419498f0df8900d30
Navy22-anchor <lss4583@gmail.com>
2025-11-30 18:24:16 +0900

    Others:그외 자잘한것

--- yeonJae/09주차/src/App.tsx
+++ yeonJae/09주차/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Homepage } from "./component/homePage";
+import { Rootlayout } from "./root-layout/layout";
+import {CartProvider} from './component/contextapi'
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Rootlayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage/>,
+      }
+    ],
+  },
+]);
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <CartProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </CartProvider>
+  );
 }
 
-export default App
+export default App;
